1195b120ef752a1e0e71d246a8ac5676db6da162
Prateek Phoenix <leavethemails@gmail.com>
2018-02-12 23:26:45 +0530

    added event details activity

---
+++ main/app/src/main/java/io/execube/monotype/phobos/EventDetailsActivity.kt
@@ -0,0 +1,12 @@
+package io.execube.monotype.phobos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EventDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_details)
+    }
+}
