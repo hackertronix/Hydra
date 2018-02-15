1462c1cec9685c47e26063136d45e4fae9b192c0
Prateek Phoenix <leavethemails@gmail.com>
2018-02-15 23:57:08 +0530

    fixed toolbar elevation issue admin app

---
+++ android-admin-client/app/src/main/java/io/execube/monotype/deimos/FeedFragment.kt
@@ -0,0 +1,19 @@
+package io.execube.monotype.deimos
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FeedFragment( ): Fragment(){
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_feed,container,false)
+    }
+
+
+
+}
--- android-admin-client/app/src/main/java/io/execube/monotype/deimos/HomeActivity.kt
+++ android-admin-client/app/src/main/java/io/execube/monotype/deimos/HomeActivity.kt
@@ -2,11 +2,23 @@ package io.execube.monotype.deimos
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.FragmentTransaction
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        swapFragment()
+        toolbar.elevation = 8.0f
+    }
+
+    private fun swapFragment() {
+
+        val fragment = FeedFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame_layout,fragment)
+        transaction.commit()
     }
 }
