acc75ddbc8db51731df7852ef26de8d9e4246b98
Prateek Phoenix <leavethemails@gmail.com>
2018-02-24 23:59:34 +0530

    added validation

--- android-admin-client/app/src/main/java/io/execube/monotype/deimos/AddEventActivity.kt
+++ android-admin-client/app/src/main/java/io/execube/monotype/deimos/AddEventActivity.kt
@@ -2,6 +2,9 @@ package io.execube.monotype.deimos
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import io.execube.monotype.deimos.model.Event
+import kotlinx.android.synthetic.main.activity_add_event.*
 
 class AddEventActivity : AppCompatActivity() {
 
@@ -9,6 +12,22 @@ class AddEventActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_event)
 
+
+        setupDefaultState()
+        validate()
+
+
+
+    }
+
+    private fun validate() {
+
+       //TODO add validation code
+    }
+
+    private fun setupDefaultState() {
+        //By default the fab will be invisible and will be spawned only on completing all fields
+            done_fab.visibility = View.GONE
 
     }
 }
