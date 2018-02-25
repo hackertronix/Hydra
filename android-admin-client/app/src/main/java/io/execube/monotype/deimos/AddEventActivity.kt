d2e7048840944d7c882378ec130dc1ff5d0fb3b7
Prateek Phoenix <leavethemails@gmail.com>
2018-02-25 23:49:20 +0530

    added date and time picker dialogs

--- android-admin-client/app/src/main/java/io/execube/monotype/deimos/AddEventActivity.kt
+++ android-admin-client/app/src/main/java/io/execube/monotype/deimos/AddEventActivity.kt
@@ -2,8 +2,17 @@ package io.execube.monotype.deimos
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import io.execube.monotype.deimos.model.Event
 import kotlinx.android.synthetic.main.activity_add_event.*
 import kotlinx.android.synthetic.main.fragment_feed.*
+import android.widget.DatePicker
+import android.app.DatePickerDialog
+import java.util.*
+import android.widget.TimePicker
+import android.app.TimePickerDialog
+
+
+
 
 class AddEventActivity : AppCompatActivity() {
 
@@ -19,6 +28,31 @@ class AddEventActivity : AppCompatActivity() {
         }
 
 
+        select_date.setOnClickListener {
+
+            val calendar = Calendar.getInstance()
+            val mYear = calendar.get(Calendar.YEAR)
+            val mMonth = calendar.get(Calendar.MONTH)
+            val mDay = calendar.get(Calendar.DAY_OF_MONTH)
+
+
+            val datePickerDialog = DatePickerDialog(this,
+                    DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth -> select_date.text = "${dayOfMonth.toString()}-${monthOfYear + 1}-$year" }, mYear, mMonth, mDay)
+            datePickerDialog.show()
+        }
+
+
+        select_time.setOnClickListener {
+            // Get Current Time
+            val c = Calendar.getInstance()
+            val mHour = c.get(Calendar.HOUR_OF_DAY)
+            val mMinute = c.get(Calendar.MINUTE)
+
+            // Launch Time Picker Dialog
+            val timePickerDialog = TimePickerDialog(this,
+                    TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute -> select_time.text = """${hourOfDay.toString()}:$minute""" }, mHour, mMinute, false)
+            timePickerDialog.show()
+        }
 
 
     }
@@ -34,12 +68,8 @@ class AddEventActivity : AppCompatActivity() {
 
         var eventName = event_name.editText?.text.toString().trim()
         var eventDescription = event_description.editText.toString().trim()
-        var eventVenue = venue_spinner.selectedItem.toString()
-        var eventCategory = category_spinner.selectedItem.toString()
-
-
-        
-
+        var eventVenue = venue_spinner.selectedItemPosition
+        var eventCategory = category_spinner.selectedItemPosition
 
 
 
