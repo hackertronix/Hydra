39eb1e8dd1f0a202f1c42e052e772e4fbbadbc19
Prateek Phoenix <leavethemails@gmail.com>
2018-04-11 23:01:02 +0530

    added navigation callbacks

--- android-admin-client/app/src/main/java/io/execube/monotype/deimos/common/HomeActivity.kt
+++ android-admin-client/app/src/main/java/io/execube/monotype/deimos/common/HomeActivity.kt
@@ -2,10 +2,12 @@ package io.execube.monotype.deimos.common
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.widget.TextView
 import io.execube.monotype.deimos.R
 import io.execube.monotype.deimos.Utils.getLinearOutSlowInInterpolator
 import io.execube.monotype.deimos.event_feed.FeedFragment
+import io.execube.monotype.deimos.photos.PhotosFragment
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
@@ -16,6 +18,24 @@ class HomeActivity : AppCompatActivity() {
 
         animateToolbar()
         swapFragment()
+
+      navigation.setOnNavigationItemSelectedListener { item ->
+        var selectedFragment: Fragment? = null
+        when (item.itemId) {
+
+          R.id.navigation_feed -> selectedFragment = FeedFragment()
+
+          R.id.navigation_photos -> selectedFragment = PhotosFragment()
+
+        //TODO change them back to their respective fragments
+          R.id.navigation_notification -> selectedFragment = PhotosFragment()
+        }
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, selectedFragment)
+        fragmentTransaction.commit()
+        true
+      }
 
     }
 
