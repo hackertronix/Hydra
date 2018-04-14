5a8e725eb8e6458e232179d6710e8fa6da6fe621
Prateek Phoenix <leavethemails@gmail.com>
2018-04-14 17:36:49 +0530

    added color field to event data

--- android-admin-client/app/src/main/java/io/execube/monotype/deimos/model/Event.kt
+++ android-admin-client/app/src/main/java/io/execube/monotype/deimos/model/Event.kt
@@ -1,9 +1,12 @@
 package io.execube.monotype.deimos.model
 
-data class Event(val eventName: String?=null,
-                 val eventId:String?=null,
-                 val eventDescription: String?=null,
-                 val eventCategory: String?=null,
-                 val eventVenue: String?=null,
-                 val eventDate: String?=null,
-                 val eventTime: String?=null)
+import java.io.Serializable
+
+data class Event(var eventName: String="",
+                 var eventId: String="",
+                 var eventDescription:  String="",
+                 var eventCategory:  String="",
+                 var eventVenue:  String="",
+                 var eventDate:  String="",
+                 var eventColor: String="",
+                 var eventTime:  String=""): Serializable
