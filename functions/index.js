84c504dca1f61facd5303cb54ef15a9ace0f947c
Prateek Phoenix <leavethemails@gmail.com>
2018-04-16 23:34:53 +0530

    notifications finalized

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,34 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Death Star!");
- console.log("Message sent from my macbook air");
+
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+// [END import]
+
+exports.pantheonNotifications = functions.firestore.document('/Notifications/{documentId}')
+    .onCreate((snap, context) => {
+
+      const original = snap.data();
+      //console.log(original);
+      const notifTitle = original.notificationTitle;
+      const description = original.notificationDescription;
+      console.log("Title is "+notifTitle+" Description is "+description);
+
+      const payload = {
+        notification: {
+            title:notifTitle,
+            body: description,
+            sound: "default"
+        },
+    };
+
+
+  console.log("Payload is: ",payload);
+
+
+  return admin.messaging().sendToTopic("pushNotifications", payload);
+ 
 });
